76a7a125bf0cfd73087b187696ea7aa97180c709
aliNassef <anassef798@gmail.com>
2024-09-23 12:54:07 +0300

    init commit create tawaq dashboard

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaswaqDashBoard());
+}
+
+class TaswaqDashBoard extends StatelessWidget {
+  const TaswaqDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container() ;
+  }
+}
