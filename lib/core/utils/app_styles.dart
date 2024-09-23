037c8fb58afc6e9a5217ecfb1c7d26dbc8811733
aliNassef <anassef798@gmail.com>
2024-09-23 13:10:17 +0300

    add fonts

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,51 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class AppStyles {
-  static final textStyle16SB = TextStyle(
-    fontSize: 16.sp,
+  static const textStyle16SB = TextStyle(
+    fontSize: 16,
     fontWeight: FontWeight.w600,
   );
-  static final textStyle14SB = TextStyle(
-    fontSize: 14.sp,
+  static const textStyle14SB = TextStyle(
+    fontSize: 14,
     fontWeight: FontWeight.w600,
   );
-  static final textStyle14M = TextStyle(
-    fontSize: 14.sp,
+  static const textStyle14M = TextStyle(
+    fontSize: 14,
     fontWeight: FontWeight.w500,
   );
-  static final textStyle14R = TextStyle(
-    fontSize: 14.sp,
+  static const textStyle14R = TextStyle(
+    fontSize: 14,
     fontWeight: FontWeight.w400,
   );
-  static final textStyle12R = TextStyle(
-    fontSize: 12.sp,
+  static const textStyle12R = TextStyle(
+    fontSize: 12,
     fontWeight: FontWeight.w400,
   );
-  static final textStyle12SB = TextStyle(
-    fontSize: 12.sp,
+  static const textStyle12SB = TextStyle(
+    fontSize: 12,
     fontWeight: FontWeight.w600,
   );
-  static final textStyle18B = TextStyle(
-    fontSize: 18.sp,
+  static const textStyle18B = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w700,
   );
 
-  static final textStyle10SB = TextStyle(
-    fontSize: 10.sp,
+  static const textStyle10SB = TextStyle(
+    fontSize: 10,
     fontWeight: FontWeight.w600,
   );
-  static final textStyle10R = TextStyle(
-    fontSize: 10.sp,
+  static const textStyle10R = TextStyle(
+    fontSize: 10,
     fontWeight: FontWeight.w400,
   );
-  static final textStyle16M = TextStyle(
-    fontSize: 16.sp,
+  static const textStyle16M = TextStyle(
+    fontSize: 16,
     fontWeight: FontWeight.w500,
   );
 
-  static final textStyle24B = TextStyle(
-    fontSize: 24.sp,
+  static const textStyle24B = TextStyle(
+    fontSize: 24,
     fontWeight: FontWeight.w700,
   );
 }
