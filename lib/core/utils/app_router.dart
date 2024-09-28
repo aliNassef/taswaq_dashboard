251f154d92c3b78fdbd3b52ef42805ccaeddccc3
aliNassef <anassef798@gmail.com>
2024-09-28 13:51:08 +0300

    cereate terms condation feature

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:taswaq_dashboard/features/home/presentation/view/home_view.dart';
 
 import '../../features/privacy_policy/presentation/view/privacy_policy_view.dart';
+import '../../features/terms_condations/presentation/view/terms_condation_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -13,7 +14,11 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const PrivacyPolicyView(),
       );
-      
+    case TermsCondationView.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const TermsCondationView(),
+      );
+
     default:
       return MaterialPageRoute(
         builder: (context) => const Scaffold(
---
+++ lib/features/terms_condations/presentation/view/terms_condation_view.dart
@@ -0,0 +1,12 @@
+ import 'package:flutter/material.dart';
+
+class TermsCondationView extends StatelessWidget {
+  const TermsCondationView({super.key});
+  static const routeName = '/terms_condation_view';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+       
+    );
+  }
+}
